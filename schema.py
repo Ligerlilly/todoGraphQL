c1f904dc564bd79e671a72d7d73d480546645a0c
Jason Lane <quizathaderat@yahoo.com>
2017-01-05 15:53:39 -0800

    Add TodoItem create and update

--- schema.py
+++ schema.py
@@ -25,6 +25,14 @@ class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_todo_lists = SQLAlchemyConnectionField(TodoList)
     todo_list = graphene.Field(TodoList, id=graphene.Int())
+    all_todo_items = SQLAlchemyConnectionField(TodoItem)
+    todo_item = graphene.Field(TodoItem, id=graphene.Int())
+
+    def resolve_todo_item(self, args, into, extra_args):
+        todos = TodoItemModel.query.all()
+        for t in todos:
+            if t.id == args.get("id"):
+                return t
 
     def resolve_todo_list(self, args, info, extra_args):
         todo_lists = TodoListModel.query.all()
@@ -54,6 +62,26 @@ class UpdateTodoList(graphene.Mutation):
                     tl.save()
                     return UpdateTodoList(todo_list=tl)
 
+class UpdateTodoItem(graphene.Mutation):
+    # Result field
+    todo_item = graphene.Field(TodoItem)
+
+    # The input fields
+    class Input:
+        id = graphene.Int()
+        name = graphene.String()
+
+    @classmethod
+    def mutate(cls, instance, args, info, extra_args):
+        todo_items = TodoItemModel.query.all()
+        if request.method == "POST":
+            name = args.get("name")
+            for t in todo_items:
+                if t.id == args.get("id"):
+                    t.name = name
+                    t.save()
+                    return UpdateTodoItem(todo_item=t)
+
 class CreateTodoListResult(graphene.Union):
     class Meta:
         types = [Success, Error]
@@ -74,10 +102,33 @@ class CreateTodoList(graphene.Mutation):
             result = Success(yeah='yeah')
             return CreateTodoList(result=result)
 
+class CreateTodoItemResult(graphene.Union):
+    class Meta:
+        types = [Success, Error]
+
+class CreateTodoItem(graphene.Mutation):
+    todo_item = graphene.Field(TodoItem)
+
+    class Input:
+        name = graphene.String(required=True)
+        todo_list_id = graphene.Int(required=True)
+
+    result = graphene.Field(CreateTodoItemResult)
+
+    @resolve_only_args
+    def mutate(self, name, todo_list_id):
+        if request.method == "POST":
+            todo_list = TodoListModel.query.get(todo_list_id)
+            todo_item = TodoItemModel(name=name, todo_list=todo_list)
+            todo_item.create()
+            result = Success(yeah='yeah')
+            return CreateTodoItem(result=result)
 
 class Mutations(graphene.ObjectType):
     updateTodoList = UpdateTodoList.Field()
     createTodoList = CreateTodoList.Field()
+    updateTodoItem = UpdateTodoItem.Field()
+    createTodoItem = CreateTodoItem.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutations)
 
@@ -88,6 +139,14 @@ allTodoLists {
       node {
         id
         name
+        todos {
+          edges {
+            node {
+              id
+              name
+            }
+          }
+        }
       }
     }
   }
@@ -119,6 +178,52 @@ mutation {
   todoList(id: 1) {
     id
     name
+  }
+}
+'''
+
+'''
+{
+  allTodoItems {
+    edges {
+      node {
+        id
+        name
+        todoList {
+          id
+          name
+        }
+      }
+    }
+  }
+}
+'''
+
+'''
+{
+  todoItem(id: 1) {
+    id
+    name
+  }
+}
+'''
+
+'''
+mutation {
+  createTodoItem(name: "new", todoListId: 1) {
+    result {
+      __typename
+    }
+  }
+}
+'''
+
+'''
+mutation M {
+  updateTodoItem(id: 9, name: "Something Different") {
+    todoItem {
+      name
+    }
   }
 }
 '''
